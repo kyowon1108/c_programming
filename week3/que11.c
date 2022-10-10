6d60e6b55b05247340c1802e1f6d88026546af18
kapr <kyowon1108@gmail.com>
2022-10-10 17:38:35 +0900

    week3 upload

--- week3/que11.c
+++ week3/que11.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
-int check_name(char name, int len) {
-
+int check_name(char (*name)[20], char * check, int len) {
+    for (int i = 0; i < len; ++i) {
+        if (!strcmp(name[i], check)) return 1;
+    }
+    return 0;
 }
 
-int input_name(char name[10][20]) {
+int input_name(char (*name)[20]) {
     int i = 0;
     while (i < 10) {
         printf("> 이름 : ");
         char a[20];
         scanf("%s", a);
-        for (int j = 0; j < i; ++j) {
-            if (!strcmp(a, *name++))
-                printf("@ 이름이 이미 등록되었습니다.");
-                break;
+        if(!strcmp(a, "end")) break;
+        if (check_name(name, a, i)) {
+            printf("@ 이름이 이미 등록되었습니다.\n");
+            continue;
         }
-        name[i] = a;
-        printf("asdf");
+        strcpy(name[i], a);
         ++i;
     }
+    return i;
 }
 
-void print_name() {
-
+void print_name(char (*name)[20], int cnt) {
+    printf("@ 총 %d명이 입력되었습니다.\n", cnt);
+    for (int i = 0; i < cnt; ++i) {
+        printf("%s\n", name[i]);
+    }
 }
 
 int main() {
@@ -31,7 +37,7 @@ int main() {
     int cnt;
 
     cnt = input_name(name);
-    //print_name(name, cnt);
+    print_name(name, cnt);
 
     return 0;
 }
